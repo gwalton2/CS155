67688299182360a67cd07def3df8b8fa192d6f1f
Grant Walton <waltogra13@gmail.com>
2018-09-03 23:32:43 -0700

    Finished homework 2 parts 1 and 2  2

---
+++ Homework2_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your first name then press enter.");
+            string first = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Please input your last name then press enter.");
+            string last = Console.ReadLine().ToLower();
+
+            string pig_first = first.Substring(1, 1).ToUpper() + first.Substring(2) + first.Substring(0, 1) + "ay";
+            string pig_last = last.Substring(1, 1).ToUpper() + last.Substring(2) + last.Substring(0, 1) + "ay";
+
+            string pig_name = pig_first + " " + pig_last;
+            Console.WriteLine(pig_name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework2_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the Fahrenheit temperature you wish to convert and press enter.");
+
+            int f_temp = Int32.Parse(Console.ReadLine());
+            double c_temp = Math.Round(5*(f_temp - 32) / (double)9, 1);
+
+            Console.WriteLine("{0} degrees Fahrenheit = {1} degrees Celsius", f_temp, c_temp);
+            Console.ReadLine();
+        }
+    }
+}
