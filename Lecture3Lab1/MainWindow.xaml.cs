1910d2e91553a99b5f9637f3299bdb16b29ff3b0
Grant Walton <waltogra13@gmail.com>
2018-09-10 20:30:19 -0700

    Finished lecture3 labs 1 and 2. 2  2

---
+++ Lecture3Lab1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture3Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, RoutedEventArgs e)
+        {
+            String pig_name = "";
+            String full_name = nameText.Text;
+
+            String[] names = nameText.Text.Split(' ');
+
+            foreach (String raw_name in names)
+            {
+                String name = raw_name.ToLower();
+                String pig;
+                if (name.Equals(""))
+                {
+                    continue;
+                }
+
+                else if (name.Length < 2)
+                {
+                    pig = name.ToUpper() + "ay";
+                }
+
+                else
+                {
+                    pig = name.Substring(1, 1).ToUpper() + name.Substring(2) + name.Substring(0, 1) + "ay";
+                }
+                pig_name += pig + " ";
+            }
+
+            pigText.Text = pig_name;
+        }
+    }
+}
---
+++ Lecture3Lab2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("So your lamp doesn't work.\nIs your lamp plugged in? Please answer Yes or No and press enter.");
+            string ans1 = Console.ReadLine().ToLower().Trim();
+
+            if (ans1.Equals("no"))
+            {
+                Console.WriteLine("Plug in your lamp");
+            }
+
+            else
+            {
+                Console.WriteLine("Is your bulb burned out? Please answer Yes or No and press enter.");
+                string ans2 = Console.ReadLine().ToLower().Trim();
+
+                if (ans2.Equals("no"))
+                {
+                    Console.WriteLine("Repair your lamp");
+                }
+
+                else
+                {
+                    Console.WriteLine("Replace the bulb");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
