6aba236f10e3ac48c57365a8fec5f0326228f126
Grant Walton <waltogra13@gmail.com>
2018-09-17 16:24:33 -0700

    Finished homework three completely! s 1 and 2. Will do tmrw

--- Homework3_1/MainWindow.xaml.cs
+++ Homework3_1/MainWindow.xaml.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/// Homework No.3 Exercise No.1
+/// File Name: MainWindow.xaml.cs
+/// @author: Grant Walton
+/// Date: Sept. 17, 2018
+/// Problem Statement: This program should use a GUI to take in a monthly mortgage payment and the outstanding balance
+/// then output the amount that goes to interest payment and the amount that goes toward the principal.
+/// Overall Plan:
+/// 1. I will first build the outline of the GUI by placing labels and textboxes for the inputs and outputs.
+/// 2. I will finish up the GUI by placing a title and a button and lining everything up.
+/// 3. Within the button's click method I will parse the payment and outstanding balance input as doubles.
+/// 4. Then I will check for two edge cases. One being if the payment is less than or equal to the interest
+/// and the other being if the payment is greater than or equal to the outstanding + interest amount.
+/// 5. The else case is obviously if the payment is between the interest and outstanding balance. In this
+/// case I will output the interest payment as the outstanding balance times the monthly rate. After subtracting
+/// this from the payment, the remaining amount will be output as the principal payment. 
+
+/// Classes needed and Purpose: Only main class is needed
+/// main class - MainWindow : Window
+/// Import necessary C# or user-defined packages
+/// Create main class and objects needed to implement task
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Homework3_2/Homework3_2.cs
+++ Homework3_2/Homework3_2.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/// Homework No.3 Exercise No.2
+/// File Name: Homework3_2.cs
+/// @author: Grant Walton
+/// Date: Sept. 17, 2018
+/// Problem Statement: This program should prompt the user to input a price between 25cents to a dollar 
+/// in 5cent increments. It should then calculate the change in quarters, dimes, and nickles based on a payment amount
+/// of 1 dollar. 
+/// Overall Plan:
+/// 1. I will print out a prompt for the user to input their price which I will parse as a double.
+/// 2. I will calculate the actual amount of change as a double and also create an integer change
+/// variable by multiplying the raw change by 100 to make my later calculations easier.
+/// 3. I will check the price to make sure it is within the constraints laid out in the prompt.
+/// 4. If the price is within the constraints I will break up the change into quarters, dimes and nickels
+/// by using integer and modular division.
+/// 5. Then I will output the results to the user. 
+
+/// Classes needed and Purpose: Only main class is needed
+/// main class - Homework3_2
+/// Import necessary C# or user-defined packages
+/// Create main class and objects needed to implement task
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +27,7 @@ using System.Threading.Tasks;
 
 namespace Homework3_2
 {
-    class Program
+    class Homework3_2
     {
         static void Main(string[] args)
         {
