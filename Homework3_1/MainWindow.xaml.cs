4c0013e06329db6b24a493df91e8227af4b0f1a4
Grant Walton <waltogra13@gmail.com>
2018-09-17 02:11:10 -0700

    Finished rough drafts of hw3 cs files to match their class names.

---
+++ Homework3_1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework3_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const double INTEREST = .005325;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            double payment = Double.Parse(paymentInput.Text);
+            double outstanding = Double.Parse(amountInput.Text);
+
+            double interest = outstanding * INTEREST;
+
+            if (payment <= interest)
+            {
+                interestOutput.Text = payment.ToString("C");
+                principalOutput.Text = "$0.00";
+            }
+
+            else if (payment >= interest + outstanding)
+            {
+                interestOutput.Text = interest.ToString("C");
+                principalOutput.Text = outstanding.ToString("C");
+            }
+
+            else
+            {
+                interestOutput.Text = interest.ToString("C");
+                principalOutput.Text = (payment - interest).ToString("C");
+            }
+        }
+    }
+}
---
+++ Homework3_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quarter = 0;
+            int dime = 0;
+            int nickle = 0;
+
+            Console.WriteLine("Enter price of item (from 25 cents to a dollar, in 5-cent increments):");
+
+            double price = Double.Parse(Console.ReadLine());
+            int change = 100 - (int)(price * 100);
+
+            if (price > 1 || price < .25 || (price * 100) % 5 != 0)
+            {
+                Console.WriteLine("The price can only be from 25 cents to a dollar in 5-cent increments.");
+                Console.WriteLine("Please try again.");
+            }
+
+            else
+            {
+                quarter = change / 25;
+                dime = (change % 25) / 10;
+                nickle = ((change % 25) % 10) / 5;
+                
+                Console.WriteLine("You bought an item for " + price.ToString("C") + " and paid a dollar. Your change is:");
+                Console.WriteLine("quarter: {0}", quarter);
+                Console.WriteLine("dime: {0}", dime);
+                Console.WriteLine("nickle: {0}", nickle);
+            }
+            Console.ReadLine();
+        }
+    }
+}
