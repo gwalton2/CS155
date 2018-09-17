d430dee8a9f8ee992702c96916a4da864f7120d0
Grant Walton <waltogra13@gmail.com>
2018-09-17 02:27:00 -0700

    Only need to add headers to hw3 parts 1 and 2. Will do tmrw

--- Homework3_2/Program.cs
+++ Homework3_2/Program.cs
@@ -17,9 +17,10 @@ namespace Homework3_2
             Console.WriteLine("Enter price of item (from 25 cents to a dollar, in 5-cent increments):");
 
             double price = Double.Parse(Console.ReadLine());
+            double raw_change = 1 - price;
             int change = 100 - (int)(price * 100);
 
-            if (price > 1 || price < .25 || (price * 100) % 5 != 0)
+            if (price > 1 || price < .25 || change % 5 != 0)
             {
                 Console.WriteLine("The price can only be from 25 cents to a dollar in 5-cent increments.");
                 Console.WriteLine("Please try again.");
@@ -31,7 +32,7 @@ namespace Homework3_2
                 dime = (change % 25) / 10;
                 nickle = ((change % 25) % 10) / 5;
                 
-                Console.WriteLine("You bought an item for " + price.ToString("C") + " and paid a dollar. Your change is:");
+                Console.WriteLine("You bought an item for " + price.ToString("C") + " and paid a dollar. Your change is " + raw_change.ToString("C") + ":");
                 Console.WriteLine("quarter: {0}", quarter);
                 Console.WriteLine("dime: {0}", dime);
                 Console.WriteLine("nickle: {0}", nickle);
