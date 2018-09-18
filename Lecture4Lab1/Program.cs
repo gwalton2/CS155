96c8219f1dc98d1570e075add503335cfc673b29
Grant Walton <waltogra13@gmail.com>
2018-09-17 18:13:55 -0700

    Finished Lab 1 for lecture 4. Speed limit lab.

---
+++ Lecture4Lab1/Program.cs
@@ -0,0 +1,44 @@
+﻿//Authors:  Grant Walton & Julie Bonsack
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture4Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fine = 0;
+
+            Console.WriteLine("Please enter a speed limit and press enter.");
+            int limit = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your speed and press enter.");
+            int speed = Int32.Parse(Console.ReadLine());
+
+            if (speed > limit)
+            {
+                fine += 60;
+
+                if (speed > (limit + 25))
+                {
+                    fine += 250;
+                }
+
+                fine += (speed - limit) * 7;
+
+                Console.WriteLine("You were speeding. Your fine is {0}.", fine);
+            }
+            else
+            {
+                Console.WriteLine("You were not speeding!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
