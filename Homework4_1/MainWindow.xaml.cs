08ac3e393330f064fc54d6776097901ed33bf681
Grant Walton <waltogra13@gmail.com>
2018-09-24 16:43:23 -0700

    Put finishing touches on hw4. All done

--- Homework4_1/MainWindow.xaml.cs
+++ Homework4_1/MainWindow.xaml.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/// Homework No.4 Exercise No.1
+/// File Name: MainWindow.xaml.cs
+/// @author: Grant Walton
+/// Date: Sept. 24, 2018
+/// Problem Statement: This program should calculate a user's bmi based off of their weight and height
+/// and also communicate if their bmi is above, below, or within a healthy range. 
+/// Overall Plan:
+/// 1. I will build a GUI that allows for the input and output of the necessary information.
+/// 2. I will take in the user's weight and height throught textboxes.
+/// 3. Once the calculate button is pressed, I will display the user's bmi in a textbox using
+/// bmi = (weight * 720)/(height^2)
+/// 4. I will also use a canvas to change color depending on their bmi. Blue if it is below the 
+/// healthy range(less than 15), green if it is within, and yellow if it is above(greater than 26).
+/// 5. In addition to the colors I will print out a message in a textbox indicating what each color
+/// means. The three possiblities will be handled with conditionals.
+
+/// Classes needed and Purpose: Only main class is needed
+/// main class - MainWindow : Window
+/// Import necessary C# or user-defined packages
+/// Create main class and objects needed to implement task
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Homework4_2/Homework4_2.cs
+++ Homework4_2/Homework4_2.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/// Homework No.4 Exercise No.2
+/// File Name: Homework4_2.cs
+/// @author: Grant Walton
+/// Date: Sept. 24, 2018
+/// Problem Statement: This program should take in 8 coin flips and print out statistics on them.
+/// Overall Plan:
+/// 1. The overall structure of the program will be a while loop that runs until the total flips hit 8.
+/// 2. Each loop will ask the user to input an h or a t for heads or tails.
+/// 3. The program will then increment heads or tails variable accordingly.
+/// 4. If the input is incorrectly formatted then the total will not be incremented and the program will loop an extra time.
+/// 5. After the loop is exited. The program will print out the total number of heads and tails and the precents associated with each.
+
+/// Classes needed and Purpose: Only main class is needed
+/// main class - Homework4_2
+/// Import necessary C# or user-defined packages
+/// Create main class and objects needed to implement task
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +23,7 @@ using System.Threading.Tasks;
 
 namespace Homework4_2
 {
-    class Program
+    class Homework4_2
     {
         static void Main(string[] args)
         {
@@ -14,21 +31,19 @@ namespace Homework4_2
             int heads = 0;
             int tails = 0;
 
-            for (int i = 0; i < 8;)
+            while (total < 8)
             {
-                Console.WriteLine("Please input an h for heads or a t for tails and press enter. {0}/8", i+1);
+                Console.WriteLine("Please input an h for heads or a t for tails and press enter. {0}/8", total+1);
                 string coin = Console.ReadLine();
 
                 if (coin.Equals("h"))
                 {
                     heads++;
-                    i++;
                     total++;
                 }
                 else if (coin.Equals("t"))
                 {
                     tails++;
-                    i++;
                     total++;
                 }
                 else
