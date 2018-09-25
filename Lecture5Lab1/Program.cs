4c1a6afeeccc62d01a9af4adecfd555b967baa4c
Grant Walton <waltogra13@gmail.com>
2018-09-24 20:49:30 -0700

    Finished labs for lecture 5.

---
+++ Lecture5Lab1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture5Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess;
+            int num_guess = 0;
+            Random generator = new Random();
+            int rand = generator.Next(0, 101);
+
+            do
+            {
+                Console.WriteLine("Enter your guess as an integer between 0 and 100");
+                guess = Int32.Parse(Console.ReadLine());
+
+                if (guess < rand)
+                {
+                    Console.WriteLine("{0} is LOWER than my number", guess);
+                }
+
+                else if (guess > rand)
+                {
+                    Console.WriteLine("{0} is HIGHER than my number", guess);
+                }
+
+                num_guess++;
+
+            } while (guess != rand);
+
+            Console.WriteLine("{0} is CORRECT! It only took you {1} guesses.", guess, num_guess);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lecture5Lab2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture5Lab2
+{
+    class Program
+    {
+        public enum Coin {Heads, Tails};
+        public const int FLIP_NUM = 10000;
+
+        static void Main(string[] args)
+        {
+            Coin[] flip_arr = new Coin[FLIP_NUM];
+            Random generator = new Random();
+
+            for (int i = 0; i < FLIP_NUM; i++)
+            {
+                Coin rand_flip = flipCoin(generator);
+                flip_arr[i] = rand_flip;
+            }
+            printFlips(flip_arr);
+        }
+
+        static Coin flipCoin(Random generator)
+        {
+            int flip = generator.Next(2);
+
+            if (flip == 1)
+            {
+                return Coin.Heads;
+            }
+            else
+            {
+                return Coin.Tails;
+            }
+        }
+
+        static void printFlips(Coin[] coin_flips)
+        {
+            int heads = 0;
+            int tails = 0;
+
+            foreach (Coin flip in coin_flips)
+            {
+                if (flip == Coin.Heads)
+                {
+                    Console.WriteLine("H");
+                    heads++;
+                }
+                else
+                {
+                    Console.WriteLine("T");
+                    tails++;
+                }
+            }
+            Console.WriteLine("For {0} flips:", FLIP_NUM);
+            Console.WriteLine("Heads: {0:p}", heads / (double)FLIP_NUM);
+            Console.WriteLine("Tails: {0:p}", tails / (double)FLIP_NUM);
+            Console.ReadLine();
+        } 
+    }
+}
