ab48df3c1cf53d71b8ade49d1fb5df16b81c1c24
Grant Walton <waltogra13@gmail.com>
2018-10-01 20:45:56 -0700

    Finished lec6 lab2

---
+++ Lecture6Lab2/Fraction.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture6Lab2
+{
+    class Fraction
+    {
+        int numerator;
+        int denominator;
+
+        public void SetNumerator(int numerator)
+        {
+            this.numerator = numerator;
+        }
+
+        public void SetDenominator(int denominator)
+        {
+            if (denominator == 0)
+            {
+                Console.WriteLine("Can't set the denominator to zero");
+            }
+            else
+            {
+                this.denominator = denominator;
+            }
+        }
+
+        public double GetDecimal()
+        {
+            return Math.Round(numerator / (double)denominator, 2);
+        }
+
+        public string SimplifyString()
+        {
+            int min;
+            int gcd = 0;
+            if (numerator == 0)
+            {
+                min = 1;
+            }
+            else
+            {
+                min = Math.Min(numerator, denominator);
+            }
+
+            for (int i = min; i >= 1; i--)
+            {
+                if (numerator % i == 0 && denominator % i == 0)
+                {
+                    gcd = i;
+                    break;
+                }
+            }
+            return (numerator / gcd).ToString() + "/" + (denominator / gcd).ToString();
+        }
+
+        public override string ToString()
+        {
+            return numerator.ToString() + "/" + denominator.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Fraction otherFrac = (Fraction)obj;
+            double var1 = otherFrac.GetDecimal();
+            double var2 = this.GetDecimal();
+            return var1 == var2;
+        }
+    }
+}
---
+++ Lecture6Lab2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture6Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the numerator and press enter. Then enter the denominator and press enter");
+            Fraction myfrac1 = new Fraction();
+            myfrac1.SetNumerator(Int32.Parse(Console.ReadLine()));
+            myfrac1.SetDenominator(Int32.Parse(Console.ReadLine()));
+            Console.WriteLine(myfrac1.ToString());
+            Console.WriteLine(myfrac1.GetDecimal().ToString());
+            Console.WriteLine(myfrac1.SimplifyString());
+
+            Console.WriteLine("Please another the numerator and press enter. Then enter another denominator and press enter");
+            Fraction myfrac2 = new Fraction();
+            myfrac2.SetNumerator(Int32.Parse(Console.ReadLine()));
+            myfrac2.SetDenominator(Int32.Parse(Console.ReadLine()));
+            Console.WriteLine(myfrac2.ToString());
+            Console.WriteLine(myfrac2.GetDecimal().ToString());
+            Console.WriteLine(myfrac2.SimplifyString());
+
+            if (myfrac1.Equals(myfrac2))
+            {
+                Console.WriteLine("these fractions are ARE equal");
+            }
+            else
+            {
+                Console.WriteLine("these fractions NOT equal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
