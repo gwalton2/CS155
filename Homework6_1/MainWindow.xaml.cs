05c27554f3686c491525c0744b70b4b0a2fb92be
Grant Walton <waltogra13@gmail.com>
2018-10-06 22:29:57 -0700

    Finished hw6 f hw6 done

--- Homework6_1/MainWindow.xaml.cs
+++ Homework6_1/MainWindow.xaml.cs
@@ -1,4 +1,35 @@
-﻿using System;
+﻿/// Homework No.6 Exercise No.1
+/// File Name: MainWindow.xaml.cs
+/// @author: Grant Walton
+/// Date: Oct. 6, 2018
+/// Problem Statement: This program should implement and test a class called Counter whose objects
+/// count positive integers in increments of 1. It can only be set to zero and it cannot count below zero.
+/// Overall Plan:
+/// 1. I will create an integer variable in the Counter class that contains the actual count.
+/// 2. I will implement the SetZero, AddOne, DecreaseOne, GetCount, PrintCount, Equals, and ToString
+/// methods of the class with simple operations on the count variable. 
+/// 3. I will build the driver program by making a GUI that contains a menu of all the methods
+/// for the Counter class and you can select each method on two different Counter objects(except for
+/// the Equals method as there will only be one option for that which compares the two objects).
+/// 4. The GUI will display the values of a string and integer variable for each object which are
+/// set to the values returned from the GetCount and ToString methods for their respective objects.
+/// 5. The PrintCount method will still print out to the Console as intended. The results of pressing 
+/// this button be seen upon closing the GUI in the console.
+/// 6. Behind the scenes, the GUI will work by creating two different Counter objects and calling 
+/// their methods for each respective button. There will be two int variables and two string 
+/// variables. One of each will be associated with a Counter object and then be set to the output
+/// of its ToString and GetCount methods. These methods will also update the GUI to display the
+/// value of the associated variable. 
+/// 7. The GUI will display True or False when the Equal button is pressed depending on the boolean
+/// returned from calling the Equal method of one Counter object with the other object as the 
+/// parameter.
+
+/// Classes needed and Purpose: Only main class is needed
+/// main class - Main Window : Window
+/// Import necessary C# or user-defined packages
+/// Create main class and objects needed to implement task
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
