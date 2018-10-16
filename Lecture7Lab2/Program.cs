4ae9a317b2ac9ed66c8a6411646739730214d3bb
Grant Walton <waltogra13@gmail.com>
2018-10-15 20:43:27 -0700

    Finished lecture 7 lab 2

---
+++ Lecture7Lab2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture7Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction();
+            f1.Numerator = 5;
+            f1.Denominator = 10;
+            Console.WriteLine(f1.ToString());
+            Console.WriteLine(f1.SimplifyString());
+            Console.WriteLine(f1.GetDecimal().ToString());
+            Console.WriteLine();
+
+            f1.Denominator += 10;
+            f1.Numerator -= 10;
+            Console.WriteLine(f1.ToString());
+            Console.WriteLine(f1.SimplifyString());
+            Console.WriteLine(f1.GetDecimal().ToString());
+            Console.WriteLine();
+
+            Fraction f2 = new Fraction();
+            f2.Numerator = 10;
+            f2.Denominator = 0;
+            f2.Denominator = 10;
+            Console.WriteLine(f2.ToString());
+            Console.WriteLine(f2.SimplifyString());
+            Console.WriteLine(f2.GetDecimal().ToString());
+            Console.WriteLine();
+
+            if (f1.Equals(f2))
+            {
+                Console.WriteLine("Fractions are equal");
+            }
+            else
+            {
+                Console.WriteLine("Fractions are NOT equal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
