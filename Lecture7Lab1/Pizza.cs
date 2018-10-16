1cd8574e3b5e829ab1e72710246963f3f382caa1
Grant Walton <waltogra13@gmail.com>
2018-10-15 19:29:12 -0700

    Finished lecture 7 lab 1

---
+++ Lecture7Lab1/Pizza.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture7Lab1
+{
+    class Pizza
+    {
+        public enum PizzaSize { small = 10, medium = 12, large = 14 };
+
+        private PizzaSize size;
+        private int cheese;
+        private int pepperoni;
+        private int ham;
+
+        public Pizza()
+        {
+            size = PizzaSize.small;
+            cheese = 0;
+            pepperoni = 0;
+            ham = 0;
+        }
+
+        public Pizza(PizzaSize size, int cheese, int pepperoni, int ham)
+        {
+            this.size = size;
+            SetCheese(cheese);
+            SetPepperoni(pepperoni);
+            SetHam(ham);
+        }
+
+        public void SetSize(PizzaSize size)
+        {
+            this.size = size;
+        }
+
+        public void SetCheese(int cheese)
+        {
+            if (cheese >= 0)
+            {
+                this.cheese = cheese;
+            }
+            else
+            {
+                this.cheese = 0;
+            }
+        }
+
+        public void SetPepperoni(int pepperoni)
+        {
+            if (pepperoni >= 0)
+            {
+                this.pepperoni = pepperoni;
+            }
+            else
+            {
+                this.pepperoni = 0;
+            }
+        }
+
+        public void SetHam(int ham)
+        {
+            if (ham >= 0)
+            {
+                this.ham = ham;
+            }
+            else
+            {
+                this.ham = 0;
+            }
+        }
+
+        public PizzaSize GetSize()
+        {
+            return size;
+        }
+
+        public int GetCheese()
+        {
+            return cheese;
+        }
+
+        public int GetPepperoni()
+        {
+            return pepperoni;
+        }
+
+        public int GetHam()
+        {
+            return ham;
+        }
+
+        public double CalculateCost()
+        {
+            double initial =  (double)size;
+            double extra = (cheese + ham + pepperoni) * 2;
+
+            return initial + extra;
+        }
+
+        public override string ToString()
+        {
+            string st_size = "Size: ";
+            switch ((int)size)
+            {
+                case 10:
+                    st_size += "small";
+                    break;
+                case 12:
+                    st_size += "medium";
+                    break;
+                case 14:
+                    st_size += "large";
+                    break;
+            }
+            string st_cheese = "Cheese: " + cheese.ToString();
+            string st_pepperoni = "Pepperoni: " + pepperoni.ToString();
+            string st_ham = "Ham: " + ham.ToString();
+            string st_cost = "Cost: " + CalculateCost().ToString("C");
+
+            return st_size + "\n" + st_cheese + "\n" + st_pepperoni + "\n" + st_ham + "\n" + st_cost;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Pizza oth_pizza = (Pizza)obj;
+            
+            if (CalculateCost() == oth_pizza.CalculateCost() && cheese == oth_pizza.GetCheese() &&
+                pepperoni == oth_pizza.GetPepperoni() && ham == oth_pizza.GetHam())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Lecture7Lab1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture7Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza p1 = new Pizza();
+
+            Console.WriteLine("Enter size(small, medium, or large) and press enter.");
+            string size = Console.ReadLine();
+            if (size.Equals("small"))
+            {
+                p1.SetSize(Pizza.PizzaSize.small);
+            }
+            else if (size.Equals("medium"))
+            {
+                p1.SetSize(Pizza.PizzaSize.medium);
+            }
+            else
+            {
+                p1.SetSize(Pizza.PizzaSize.large);
+            }
+
+            Console.WriteLine("Enter the number of cheese toppings as an integer and press enter");
+            p1.SetCheese(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Enter the number of pepperoni toppings as an integer and press enter");
+            p1.SetPepperoni(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Enter the number of ham toppings as an integer and press enter");
+            p1.SetHam(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Pizza 1:\n" + p1.ToString());
+            Console.WriteLine();
+
+            Pizza p2 = new Pizza(Pizza.PizzaSize.medium, 5, 10, 2);
+
+            Console.WriteLine(p2.GetHam().ToString());
+            Console.WriteLine(p2.GetPepperoni().ToString());
+            Console.WriteLine(p2.GetCheese().ToString());
+            Console.WriteLine(p2.CalculateCost().ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("Pizza 2:\n" + p2.ToString());
+            Console.WriteLine();
+
+            if (p1.Equals(p2))
+            {
+                Console.WriteLine("the pizzas are equal!");
+            }
+            else
+            {
+                Console.WriteLine("the pizzas are not equal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
