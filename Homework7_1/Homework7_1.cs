e572b2bea286b4cf3cd58e86849e3655c4fa30af
Grant Walton <waltogra13@gmail.com>
2018-10-22 15:42:24 -0700

    Finished meat of hw7_1 2

---
+++ Homework7_1/Homework7_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7_1
+{
+    class Homework7_1
+    {
+        static void Main(string[] args)
+        {
+            Odometer od1 = new Odometer();
+
+            Console.WriteLine("Please enter the miles and press enter and then the fuel used and press enter.");
+            Odometer od2 = new Odometer(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            Console.WriteLine(od2.ToString());
+
+            od1.Add(32, 10);
+            Console.WriteLine("other odometer: {0}", od1.ToString());
+            od1.Reset();
+
+            Console.WriteLine("Please enter the miles you would like to add and press enter. Then do the same for fuel used.");
+            od2.Add(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            Console.WriteLine(od2.ToString());
+
+            Console.WriteLine("other odometer after resetting: {0}", od1.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework7_1/Odometer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7_1
+{
+    class Odometer
+    {
+        private int milage;
+        private int fuel_usage;
+
+        public Odometer()
+        {
+            milage = 0;
+            fuel_usage = 0;
+        }
+
+        public Odometer(int milage, int fuel_usage)
+        {
+            this.milage = milage;
+            this.fuel_usage = fuel_usage;
+        }
+
+        public void Reset()
+        {
+            milage = 0;
+            fuel_usage = 0;
+        }
+
+        public void Add(int miles, int fuel)
+        {
+            milage += miles;
+            fuel_usage += fuel;
+        }
+
+        public double MilesPerGallon()
+        {
+            if (fuel_usage == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return milage / (double)fuel_usage;
+            }
+        }
+
+        public override string ToString()
+        {
+            return milage.ToString() + "miles " + fuel_usage.ToString() + "gallons: " + MilesPerGallon().ToString() + "mpg";
+        }
+    }
+}
