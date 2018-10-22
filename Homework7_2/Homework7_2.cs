716a67abfee04d8915eecc4a1099a8381639f254
Grant Walton <waltogra13@gmail.com>
2018-10-22 16:56:46 -0700

    Finished meat of hw7_2

---
+++ Homework7_2/Homework7_2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7_2
+{
+    class Homework7_2
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp1 = new Temperature();
+            Temperature temp2;
+            Temperature temp3;
+            Temperature temp4 = new Temperature(0, 'F');
+
+            Console.WriteLine("Enter the scale, either F or C, you would like temp2 to be in and press enter");
+            char[] scale2 = Console.ReadLine().ToCharArray();
+            temp2 = new Temperature(scale2[0]);
+
+            Console.WriteLine("Enter the temperature you would like temp3 to be and press enter");
+            temp3 = new Temperature(Double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Enter the scale for temp1 and press enter. Do the same for its temperature");
+            char[] scale1 = Console.ReadLine().ToCharArray();
+            temp1.SetBoth(Double.Parse(Console.ReadLine()), scale1[0]);
+
+            Console.WriteLine("Enter the temperature you would like temp4 to be at and press enter");
+            temp4.SetTemperature(Double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Enter the scale you would like temp3 to be in and press enter");
+            char[] scale3 = Console.ReadLine().ToCharArray();
+            temp3.SetScale(scale3[0]);
+
+            Console.WriteLine("temp1: {0} \n{1}C \n{2}F", temp1.ToString(), temp1.GetTemperatureC().ToString(), temp1.GetTemperatureF().ToString());
+            Console.WriteLine("temp3: {0} \n{1}C \n{2}F", temp3.ToString(), temp3.GetTemperatureC().ToString(), temp3.GetTemperatureF().ToString());
+
+            Console.WriteLine("temp1 is equal to temp2:");
+            if (temp1.Equals(temp2))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.WriteLine("temp3 is equal to temp4:");
+            if (temp3.Equals(temp4))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework7_2/Temperature.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7_2
+{
+    class Temperature
+    {
+        private double temperature;
+        private char scale;
+
+        public Temperature()
+        {
+            temperature = 0;
+            scale = 'C';
+        }
+
+        public Temperature(double temperature)
+        {
+            SetTemperature(temperature);
+        }
+
+        public Temperature(char scale)
+        {
+            SetScale(scale);
+        }
+
+        public Temperature(double temperature, char scale)
+        {
+            SetTemperature(temperature);
+            SetScale(scale);
+        }
+
+        public void SetScale(char scale)
+        {
+            if (scale.Equals('F'))
+            {
+                this.scale = scale;
+            }
+            else
+            {
+                this.scale = 'C';
+            }
+        }
+
+        public void SetTemperature(double temperature)
+        {
+            this.temperature = temperature;
+        }
+
+        public void SetBoth(double temperature, char scale)
+        {
+            SetScale(scale);
+            SetTemperature(temperature);
+        }
+
+        public double GetTemperatureF()
+        {
+            if (scale.Equals('F'))
+            {
+                return Math.Round(temperature, 1);
+            }
+            else
+            {
+                double faren = (temperature * 1.8) + 32;
+                return Math.Round(faren, 1);
+            }
+        }
+
+        public double GetTemperatureC()
+        {
+            if (scale.Equals('C'))
+            {
+                return Math.Round(temperature, 1);
+            }
+            else
+            {
+                double cels = (temperature - 32) / 1.8;
+                return Math.Round(cels, 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            return temperature.ToString() + scale;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Temperature otherTemp = (Temperature)obj;
+
+            double mytemp = Math.Round(temperature, 1);
+            double othertemp;
+
+            if (scale.Equals('F'))
+            {
+                othertemp = otherTemp.GetTemperatureF();
+            }
+            else
+            {
+                othertemp = otherTemp.GetTemperatureC();
+            }
+
+            return mytemp == othertemp;
+        }
+    }
+}
