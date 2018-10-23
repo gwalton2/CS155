85dd5b927e2eda84d0f384095ea8e676bcf81f39
Grant Walton <waltogra13@gmail.com>
2018-10-22 18:56:09 -0700

    Finished lec8 lab1

---
+++ Lecture8Lab1/Fraction.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture8Lab1
+{
+    struct Fraction
+    {
+        private int _numerator;
+        private int _denominator;
+
+        public int Numerator
+        {
+            get
+            {
+                return _numerator;
+            }
+            set
+            {
+                _numerator = value;
+            }
+        }
+
+        public int Denominator
+        {
+            get
+            {
+                return _denominator;
+            }
+            set
+            {
+                if (value == 0)
+                {
+                    Console.WriteLine("Can't set denominator to zero");
+                }
+                else
+                {
+                    _denominator = value;
+                }
+            }
+        }
+
+        public double GetDecimal()
+        {
+            return Math.Round(_numerator / (double)_denominator, 2);
+        }
+
+        public string SimplifyString()
+        {
+            int min;
+            int gcd = 0;
+            if (_numerator == 0)
+            {
+                min = 1;
+            }
+            else
+            {
+                min = Math.Min(Math.Abs(_numerator), Math.Abs(_denominator));
+            }
+
+            for (int i = min; i >= 1; i--)
+            {
+                if (_numerator % i == 0 && _denominator % i == 0)
+                {
+                    gcd = i;
+                    break;
+                }
+            }
+            return (_numerator / gcd).ToString() + "/" + (_denominator / gcd).ToString();
+        }
+
+        public override string ToString()
+        {
+            return _numerator.ToString() + "/" + _denominator.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Fraction otherFrac = (Fraction)obj;
+            double var1 = otherFrac.GetDecimal();
+            double var2 = this.GetDecimal();
+            return var1 == var2;
+        }
+    }
+}
---
+++ Lecture8Lab1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture8Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction();
+            Console.WriteLine("Input the numerator and press enter. Do the same for the denominator");
+            f1.Numerator = Int32.Parse(Console.ReadLine());
+            f1.Denominator = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(f1.ToString());
+            Console.WriteLine(f1.SimplifyString());
+            Console.WriteLine(f1.GetDecimal().ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("Input the amount to increase the numerator by and press enter. Do the same for the denominator");
+            UpdateFraction(f1, Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            Console.WriteLine("Withing Main: {0}", f1.ToString());
+
+            Fraction f2 = new Fraction();
+            Console.WriteLine("Input the numerator and press enter. Do the same for the denominator");
+            f2.Numerator = Int32.Parse(Console.ReadLine());
+            f2.Denominator = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(f2.ToString());
+            Console.WriteLine(f2.SimplifyString());
+            Console.WriteLine(f2.GetDecimal().ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("Input the amount to increase the numerator by and press enter. Do the same for the denominator");
+            UpdateFraction(f2, Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            Console.WriteLine("Withing Main: {0}", f2.ToString());
+
+            if (f1.Equals(f2))
+            {
+                Console.WriteLine("Fractions are equal");
+            }
+            else
+            {
+                Console.WriteLine("Fractions are NOT equal");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void UpdateFraction(Fraction frac, int numerator, int denominator)
+        {
+            frac.Denominator += denominator;
+            frac.Numerator += numerator;
+
+            Console.WriteLine("Within UpdateFraction:");
+            Console.WriteLine(frac.ToString());
+            Console.WriteLine(frac.SimplifyString());
+            Console.WriteLine(frac.GetDecimal().ToString());
+            Console.WriteLine();
+        }
+    }
+}
