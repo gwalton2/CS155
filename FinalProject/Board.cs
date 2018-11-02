4c1f0da2a954eed618efb52bf973ee1ebedc4d7d
Grant Walton <waltogra13@gmail.com>
2018-11-02 03:13:34 -0700

    Implemented methods to get all possible moves for each piece in the Moves class and implemented sume fields and logic in the board class. Also made the bare bones of the GUI.

---
+++ FinalProject/Board.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Board
+    {
+        ulong _whitepawns;
+        ulong _whiteknights;
+        ulong _whitebishops;
+        ulong _whiterooks;
+        ulong _whitequeen;
+        ulong _whiteking;
+
+        ulong _blackpawns;
+        ulong _blackknights;
+        ulong _blackbishops;
+        ulong _blackrooks;
+        ulong _blackqueen;
+        ulong _blackking;
+
+        ulong _allwhite;
+        ulong _allblack;
+        ulong _allpieces;
+
+        char[,] chessboard = new char[8, 8] { { 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' }, //uppercase is white
+                                              { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
+                                              { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                                              { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }, //Board reversed visually to match index
+                                              { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                                              { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+                                              { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
+                                              { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' } }; // lowercase is black
+
+        public ulong AllPieces { get; }
+
+        public ulong GetMoves(int rank, int file)
+        {
+            char piece = chessboard[rank, file];
+
+            switch (piece)
+            {
+                case 'r':
+                    return Moves.GetRookMoves(rank, file, _allpieces, _allblack);
+
+                case 'n':
+                    return Moves.GetKnightMoves(rank, file, _allblack);
+
+                case 'b':
+                    return Moves.GetBishopMoves(rank, file, _allpieces, _allblack);
+
+                case 'q':
+                    return Moves.GetQueenMoves(rank, file, _allpieces, _allblack);
+
+                case 'k':
+                    return Moves.GetKingMoves(rank, file, _allblack);
+
+                case 'p':
+                    return Moves.GetBlackPawnMoves(rank, file, _allpieces, _allwhite);
+
+                case 'R':
+                    return Moves.GetRookMoves(rank, file, _allpieces, _allwhite);
+
+                case 'N':
+                    return Moves.GetKnightMoves(rank, file, _allwhite);
+
+                case 'B':
+                    return Moves.GetBishopMoves(rank, file, _allpieces, _allwhite);
+
+                case 'Q':
+                    return Moves.GetQueenMoves(rank, file, _allpieces, _allwhite);
+
+                case 'K':
+                    return Moves.GetKingMoves(rank, file, _allwhite);
+
+                case 'P':
+                    return Moves.GetWhitePawnMoves(rank, file, _allpieces, _allblack);
+
+                default:
+                    return 0;
+            }
+        }
+
+    }
+}
---
+++ FinalProject/Moves.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Moves
+    {
+        static ulong[] rankmasks = { 0xFF, 0xFF00, 0xFF0000, 0xFF000000, 0xFF00000000, 0xFF0000000000,
+                                     0xFF000000000000, 0xFF00000000000000 }; //From bottom to top
+
+        static ulong[] filemasks = { 0x101010101010101, 0x202020202020202, 0x404040404040404, 0x808080808080808,
+                                     0x1010101010101010, 0x2020202020202020, 0x4040404040404040, 0x8080808080808080 }; //From left to right
+
+        static ulong[] diagonalmasks = { 0x100000000000000, 0x201000000000000, 0x402010000000000, 0x804020100000000,
+                                         0x1008040201000000, 0x2010080402010000, 0x4020100804020100, 0x8040201008040201,
+                                         0x80402010080402, 0x804020100804, 0x8040201008, 0x80402010, 0x804020, 0x8040, 0x80 }; //Top left to bottom right corner
+
+        static ulong[] adiagonalmasks = { 0x8000000000000000, 0x4080000000000000, 0x2040800000000000, 0x1020408000000000,
+                                          0x810204080000000, 0x408102040800000, 0x204081020408000, 0x102040810204080,
+                                          0x1020408102040, 0x10204081020, 0x102040810, 0x1020408, 0x10204, 0x102, 0x1 }; //Top right to bottom left corner
+
+        static ulong clearA = 0xFEFEFEFEFEFEFEFE;
+        static ulong clearB = 0xFDFDFDFDFDFDFDFD;
+        static ulong clearH = 0x7F7F7F7F7F7F7F7F;
+        static ulong clearG = 0xBFBFBFBFBFBFBFBF;
+
+        public static ulong Reverse(ulong orig)
+        {
+            ulong rev = 0;
+            while (orig != 0)
+            {
+                rev <<= 1;
+                rev ^= orig & 1;
+                orig >>= 1;
+            }
+            return rev;
+        }
+
+        public static ulong SlidingMoves(int rank, int file, ulong mask, ulong occupied, ulong piececolor)
+        {
+            int index = rank * 8 + file; //Converts rank and file into index: A1 = 0, H8 = 63
+            ulong s = (ulong)(0x1 << index); //Creates bitboard consisting only of selected piece
+            ulong o = (occupied - s) & mask; //Creates final o variable by removing piece and focusing on mask
+
+            ulong unfiltered = ((o - 2 * s) ^ Reverse(Reverse(o) - 2 * Reverse(s))) & mask; //hyperbola quintessance algorithm
+            return unfiltered & ~piececolor; //Trims off capturing of own pieces
+        }
+
+        public static ulong GetBishopMoves(int rank, int file, ulong occupied, ulong piececolor)
+        {
+            int index = rank * 8 + file;
+            ulong mask = diagonalmasks[(index % 8) + 7 - (index / 8)] | adiagonalmasks[(index / 8) + (index % 8)];
+            return SlidingMoves(rank, file, mask, occupied, piececolor);
+        }
+
+        public static ulong GetRookMoves(int rank, int file, ulong occupied, ulong piececolor)
+        {
+            int index = rank * 8 + file;
+            ulong mask = rankmasks[index / 8] | filemasks[index % 8];
+            return SlidingMoves(rank, file, mask, occupied, piececolor);
+        }
+
+        public static ulong GetQueenMoves(int rank, int file, ulong occupied, ulong piececolor)
+        {
+            int index = rank * 8 + file;
+            ulong mask = rankmasks[index / 8] | filemasks[index % 8] | diagonalmasks[(index % 8) + 7 - (index / 8)] | adiagonalmasks[(index / 8) + (index % 8)];
+            return SlidingMoves(rank, file, mask, occupied, piececolor);
+        }
+
+        public static ulong GetKingMoves(int rank, int file, ulong piececolor)
+        {
+            int index = rank * 8 + file;
+            ulong piece = (ulong)(0x1 << index);
+
+            ulong spot1 = (piece & clearA) << 7;
+            ulong spot2 = piece << 8;
+            ulong spot3 = (piece & clearH) << 9;
+            ulong spot4 = (piece & clearH) << 1;
+            ulong spot5 = (piece & clearH) >> 7;
+            ulong spot6 = piece >> 8;
+            ulong spot7 = (piece & clearA) >> 9;
+            ulong spot8 = (piece & clearA) >> 1;
+
+            ulong kingmoves = spot1 | spot2 | spot3 | spot4 | spot5 | spot6 | spot7 | spot8;
+            return kingmoves & ~piececolor;
+        }
+
+        public static ulong GetKnightMoves(int rank, int file, ulong piececolor)
+        {
+            int index = rank * 8 + file;
+            ulong piece = (ulong)(0x1 << index);
+
+            ulong spot1 = (piece & clearA & clearB) << 6;
+            ulong spot2 = (piece & clearA) << 15;
+            ulong spot3 = (piece & clearH) << 17;
+            ulong spot4 = (piece & clearH & clearG) << 10;
+            ulong spot5 = (piece & clearH & clearG) >> 6;
+            ulong spot6 = (piece & clearH) >> 15;
+            ulong spot7 = (piece & clearA) >> 17;
+            ulong spot8 = (piece & clearA & clearB) >> 10;
+
+            ulong knightmoves = spot1 | spot2 | spot3 | spot4 | spot5 | spot6 | spot7 | spot8;
+            return knightmoves & ~piececolor;
+        }
+
+        public static ulong GetWhitePawnMoves(int rank, int file, ulong occupied, ulong othercolor)
+        {
+            int index = rank * 8 + file;
+            ulong piece = (ulong)(0x1 << index);
+
+            ulong one_step = (piece << 8) & ~occupied;
+            ulong two_step = (one_step << 8) & ~occupied;
+            ulong pawnmoves = one_step | two_step;
+
+            ulong left_attack = (piece & clearA) << 7;
+            ulong right_attack = (piece & clearH) << 9;
+            ulong pawn_attacks = (left_attack | right_attack) & othercolor;
+
+            return pawnmoves | pawn_attacks;
+        }
+
+        public static ulong GetBlackPawnMoves(int rank, int file, ulong occupied, ulong othercolor)
+        {
+            int index = rank * 8 + file;
+            ulong piece = (ulong)(0x1 << index);
+
+            ulong one_step = (piece >> 8) & ~occupied;
+            ulong two_step = (one_step >> 8) & ~occupied;
+            ulong pawnmoves = one_step | two_step;
+
+            ulong left_attack = (piece & clearA) >> 9;
+            ulong right_attack = (piece & clearH) >> 7;
+            ulong pawn_attacks = (left_attack | right_attack) & othercolor;
+
+            return pawnmoves | pawn_attacks;
+        }
+    }
+}
