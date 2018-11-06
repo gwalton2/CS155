f0ccd186ac267b568185a525236cc2b966849edc
Grant Walton <waltogra13@gmail.com>
2018-11-05 19:34:42 -0800

    Finished lab10. Added screenshot to hw9

---
+++ Lecture10Lab1/PhoneBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture10Lab1
+{
+    class PhoneBook
+    {
+        private Dictionary<string, int> book;
+
+        public PhoneBook()
+        {
+            book = new Dictionary<string, int>();
+        }
+
+        public void Add(string name, int number)
+        {
+            book.Add(name, number);
+        }
+
+        public int FindNumber(string name)
+        {
+            return book[name];
+        }
+
+        public void Delete(string name)
+        {
+            book.Remove(name);
+        }
+
+        public override string ToString()
+        {
+            string str_book = "";
+            foreach(KeyValuePair<string, int> entry in book)
+            {
+                str_book += entry.Key + ": " + entry.Value + "\n";
+            }
+
+            return str_book;
+        }
+    }
+}
---
+++ Lecture10Lab1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture10Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBook mybook = new PhoneBook();
+            mybook.Add("Grant", 2607789);
+            mybook.Add("Bob", 7858093);
+            mybook.Add("Joe", 8993456);
+            mybook.Add("Mom", 9998888);
+
+            Console.WriteLine(mybook.ToString());
+            Console.WriteLine();
+
+            
+
+            Console.WriteLine("Type 'Add' to add a phone number, 'Find' to find a current number, or 'Delete' to delete a current number");
+            string input = Console.ReadLine();
+
+            if (input.Equals("Add"))
+            {
+                Console.WriteLine("Type the name you want to add and press enter. Then enter their phone number and press enter");
+                string name = Console.ReadLine();
+                int number = Int32.Parse(Console.ReadLine());
+                mybook.Add(name, number);
+                Console.WriteLine(mybook.ToString());
+            }
+            else if (input.Equals("Find"))
+            {
+                Console.WriteLine("Type the name of whose number you would like to find and press enter");
+                string name = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine(mybook.FindNumber(name));
+                }
+                catch (KeyNotFoundException)
+                {
+                    Console.WriteLine(name + " is not in phonebook");
+                }
+
+            }
+            else if (input.Equals("Delete"))
+            {
+                Console.WriteLine("Type the name the person you'd like to delete and press enter");
+                string name = Console.ReadLine();
+                mybook.Delete(name);
+                Console.WriteLine(mybook.ToString());
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
