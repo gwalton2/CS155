6e12c1bcdf1038f859326ec1d83d5ea98de2e347
Grant Walton <waltogra13@gmail.com>
2018-11-26 01:58:59 -0800

    Finished two classes for hw11. Just need to do the file reading/writing. me class for tracking whose turn it is. s.

---
+++ Homework_11/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11
+{
+    class Student : IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Score { get; set; }
+
+        public Student(string first, string last, int score)
+        {
+            FirstName = first;
+            LastName = last;
+            Score = score;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Student other = (Student)obj;
+            return Score - other.Score;
+        }
+    }
+}
---
+++ Homework_11/StudentStats.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11
+{
+    class StudentStats
+    {
+        private List<Student> students;
+
+        public StudentStats()
+        {
+            students = new List<Student>();
+        }
+
+        public void Add(Student student)
+        {
+            students.Add(student);
+        }
+
+        public double Average()
+        {
+            int total = 0;
+            foreach (Student student in students)
+            {
+                total += student.Score;
+            }
+            return total / (double)students.Count;
+        }
+
+        public double Median()
+        {
+            int length = students.Count;
+
+            if (students.Count % 2 == 0)
+            {
+                return (students[length / 2].Score + students[(length / 2) + 1].Score) / (double)2;
+            }
+            else
+            {
+                return students[(length / 2) + 1].Score;
+            }
+        }
+    }
+}
